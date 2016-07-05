d106928a013c0c97a07dec28481fac0336d48f9a
chaynikova <chaynikova@gmail.com>
2016-07-05 12:00:59 +0300

    Test

---
+++ src/Hero.java
@@ -0,0 +1,18 @@
+/**
+ * Created by t.chaynikova on 7/5/2016.
+ */
+public class Hero {
+
+  private  String name;
+  private  int strength;
+
+  public static void main(String [ ] args){
+
+    Hero firstHero = new Hero();
+    Hero secondHero = new Hero();
+    Hero thirdHero = new Hero();
+
+  }
+
+
+}
---
+++ src/Weapon.java
@@ -0,0 +1,7 @@
+/**
+ * Created by t.chaynikova on 7/5/2016.
+ */
+public class Weapon {
+
+  private int efficiency;
+}
