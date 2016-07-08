72c9310b90748014ee879dd557424a2dc933f3ee
chaynikova <chaynikova@gmail.com>
2016-07-08 13:07:38 +0300

    Subclass Wizard is added.

---
+++ src/Magic.java
@@ -0,0 +1,25 @@
+/**
+ * Created by t.chaynikova on 7/8/2016.
+ */
+public class Magic {
+
+  private String type;
+
+  public int getPower() {
+    return power;
+  }
+
+  public void setPower(int power) {
+    this.power = power;
+  }
+
+  private int power;
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+}
---
+++ src/Wizard.java
@@ -0,0 +1,17 @@
+/**
+ * Created by t.chaynikova on 7/8/2016.
+ */
+public class Wizard extends Hero {
+
+  private Magic magic;
+
+  public Wizard (String name, int strength, Magic magic){
+
+    super(name, strength);
+    this.magic = magic;
+
+
+  }
+
+
+}
